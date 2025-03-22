5212f590a65ede5cd8ab25719978dc83d9e1478b
hachi-hatchi <yr78.kim@gmail.com>
2025-03-22 23:22:03 +0900

    Eloquent ORMを使ってデータを取得しようの課題を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,5 +14,10 @@ class PostController extends Controller
         $posts = DB::table('posts')->select('title', 'content')->get();
 
         return view('posts.index', compact('posts'));
+    }
+
+    public function show($id) {
+        $posts = Post::find($id);
+        return view('posts.show', compact('posts'));
     }
 }
